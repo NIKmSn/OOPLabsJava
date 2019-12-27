71ad9c631a4d3fc5e1f31a313a25e3961460d75b
NIK <nekit.gorak@gmail.com>
2019-12-27 03:26:56 +0300

    Prac 2 done

---
+++ Prac2/src/Project1/TestAuthor.java
@@ -0,0 +1,8 @@
+package Project1;
+
+public class TestAuthor {
+    public static void main(String[] args){
+        Author tolstoy = new Author("Лев Николаевич Толстой", "Leo@tolstoy.com", 'M');
+        System.out.println(tolstoy);
+    }
+}
