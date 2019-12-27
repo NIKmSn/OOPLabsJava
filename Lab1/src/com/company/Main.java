26b34cc2d291c9b4b5c9ef401d6fa0247e4a0e0b
NIK <nekit.gorak@gmail.com>
2019-12-27 03:25:38 +0300

    Lab 1 done

--- Lab1/src/com/company/Main.java
+++ Lab1/src/com/company/Main.java
@@ -4,19 +4,17 @@ import java.math.BigInteger;
 
 public class Main {
 
-    public static BigInteger Factorial(int n)
-    {
+    public static BigInteger Factorial(int n) {
         BigInteger result = BigInteger.ONE;
-        for (int i = 1; i <= n; ++i)
-        {
+        for (int i = 1; i <= n; ++i) {
             result = result.multiply(BigInteger.valueOf(i));
         }
         return result;
     }
+
     public static void main(String[] args) {
-    for (int i = 1; i<=10; ++i)
-    {
-        System.out.println(String.format("Factorial of %d: %d", i, Factorial(i)));
-    }
+        for (int i = 1; i <= 10; ++i) {
+            System.out.println(String.format("Factorial of %d: %d", i, Factorial(i)));
+        }
     }
 }
